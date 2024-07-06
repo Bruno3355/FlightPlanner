aa4e475573f895a7aeb1b85f536665535bba3bf9
“HanzyShadow” <brunohdossantos3@hotmail.com>
2024-07-05 21:15:52 -0300

    Minor fixes

--- src/app/lib/stripes/stripes.tsx
+++ src/app/lib/stripes/stripes.tsx
@@ -4,36 +4,36 @@ export default function Stripes() {
 
   return (
     <div className="h-screen w-full relative grid justify-between align-middle">
-<span style={{ ['--i' as any]: 27,}}></span>
-<span style={{ ['--i' as any]: 32,}}></span>
-<span style={{ ['--i' as any]: 19,}}></span>
-<span style={{ ['--i' as any]: 21,}}></span>
-<span style={{ ['--i' as any]: 36,}}></span>
-<span style={{ ['--i' as any]: 11,}}></span>
-<span style={{ ['--i' as any]: 24,}}></span>
-<span style={{ ['--i' as any]: 18,}}></span>
-<span style={{ ['--i' as any]: 39,}}></span>
-<span style={{ ['--i' as any]: 25,}}></span>
-<span style={{ ['--i' as any]: 14,}}></span>
-<span style={{ ['--i' as any]: 29,}}></span>
-<span style={{ ['--i' as any]: 12,}}></span>
-<span style={{ ['--i' as any]: 20,}}></span>
-<span style={{ ['--i' as any]: 35,}}></span>
-<span style={{ ['--i' as any]: 28,}}></span>
-<span style={{ ['--i' as any]: 30,}}></span>
-<span style={{ ['--i' as any]: 22,}}></span>
-<span style={{ ['--i' as any]: 33,}}></span>
-<span style={{ ['--i' as any]: 13,}}></span>
-<span style={{ ['--i' as any]: 31,}}></span>
-<span style={{ ['--i' as any]: 23,}}></span>
-<span style={{ ['--i' as any]: 34,}}></span>
-<span style={{ ['--i' as any]: 26,}}></span>
-<span style={{ ['--i' as any]: 15,}}></span>
-<span style={{ ['--i' as any]: 38,}}></span>
-<span style={{ ['--i' as any]: 16,}}></span>
-<span style={{ ['--i' as any]: 37,}}></span>
-<span style={{ ['--i' as any]: 17,}}></span>
-<span style={{ ['--i' as any]: 10,}}></span>
+      <span style={{ ['--i' as any]: 27, }}></span>
+      <span style={{ ['--i' as any]: 32, }}></span>
+      <span style={{ ['--i' as any]: 19, }}></span>
+      <span style={{ ['--i' as any]: 21, }}></span>
+      <span style={{ ['--i' as any]: 36, }}></span>
+      <span style={{ ['--i' as any]: 11, }}></span>
+      <span style={{ ['--i' as any]: 24, }}></span>
+      <span style={{ ['--i' as any]: 18, }}></span>
+      <span style={{ ['--i' as any]: 39, }}></span>
+      <span style={{ ['--i' as any]: 25, }}></span>
+      <span style={{ ['--i' as any]: 14, }}></span>
+      <span style={{ ['--i' as any]: 29, }}></span>
+      <span style={{ ['--i' as any]: 12, }}></span>
+      <span style={{ ['--i' as any]: 20, }}></span>
+      <span style={{ ['--i' as any]: 35, }}></span>
+      <span style={{ ['--i' as any]: 28, }}></span>
+      <span style={{ ['--i' as any]: 30, }}></span>
+      <span style={{ ['--i' as any]: 22, }}></span>
+      <span style={{ ['--i' as any]: 33, }}></span>
+      <span style={{ ['--i' as any]: 13, }}></span>
+      <span style={{ ['--i' as any]: 31, }}></span>
+      <span style={{ ['--i' as any]: 23, }}></span>
+      <span style={{ ['--i' as any]: 34, }}></span>
+      <span style={{ ['--i' as any]: 26, }}></span>
+      <span style={{ ['--i' as any]: 15, }}></span>
+      <span style={{ ['--i' as any]: 38, }}></span>
+      <span style={{ ['--i' as any]: 16, }}></span>
+      <span style={{ ['--i' as any]: 37, }}></span>
+      <span style={{ ['--i' as any]: 17, }}></span>
+      <span style={{ ['--i' as any]: 10, }}></span>
 
 
     </div>
